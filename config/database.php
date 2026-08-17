203187a84b0197f6fe6a1491e8ced47155eeabe2
zaazhafrnn <zaazhafrnn@gmail.com>
2026-08-17 16:22:13 +0700

    update config

--- config/database.php
+++ config/database.php
@@ -3,7 +3,7 @@
 return [
     'host' => '127.0.0.1',
     'dbname' => 'automationweek_9',
-    'username' => 'root',
-    'password' => 'password',
+    'username' => 'autg6277',
+    'password' => 'zgeSpeGAmTDB47',
     'charset' => 'utf8mb4'
 ];
--- schema.sql
+++ schema.sql
@@ -1,7 +1,3 @@
-CREATE DATABASE IF NOT EXISTS automationweek_9 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
-USE automationweek_9;
-
 CREATE TABLE IF NOT EXISTS accounts (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
